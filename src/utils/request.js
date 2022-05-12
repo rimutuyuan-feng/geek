b2e86a0ef3b92705000f39a4289899b9af5adc77
riddle <482825911@qq.com>
2022-05-12 16:55:05 +0800

    feat: 添加响应拦截的错误处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { hasToken, getToken, removeToken } from './storage'
+import { message } from 'antd'
+import history from './history'
 const instance = axios.create({
 	baseURL: 'http://geek.itheima.net/v1_0/',
 	timeout: 3000,
@@ -8,6 +11,9 @@ const instance = axios.create({
 instance.interceptors.request.use(
 	function (config) {
 		// 在发送请求之前做些什么
+		if (hasToken()) {
+			config.headers.Authorization = `Bearer ${getToken()}`
+		}
 		return config
 	},
 	function (error) {
@@ -21,11 +27,16 @@ instance.interceptors.response.use(
 	function (response) {
 		// 2xx 范围内的状态码都会触发该函数。
 		// 对响应数据做点什么
-		return { ...response, ...response.data }
+		return response.data
 	},
 	function (error) {
 		// 超出 2xx 范围的状态码都会触发该函数。
 		// 对响应错误做点什么
+		if (error.response.status === 401) {
+			removeToken()
+			message.warn('登录信息过期了', 1)
+			history.location.push('/login')
+		}
 		return Promise.reject(error)
 	}
 )
