c057f4fa4e02241408a4a5338c062ef21a9a1792
riddle <482825911@qq.com>
2022-05-12 15:07:12 +0800

    faat: 自定义AuthRoute

---
+++ src/components/AuthRoute/index.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { hasToken } from 'utils/storage'
+export default class AuthRoute extends Component {
+	render() {
+		const { component: Component, ...rest } = this.props
+		return (
+			<Route
+				{...rest}
+				render={(props) => {
+					if (hasToken()) {
+						return <Component {...props} />
+					} else {
+						return <Redirect to='/login' />
+					}
+				}}
+			/>
+		)
+	}
+}
