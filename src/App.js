774dc606ec37ba7c76ca3b975acae29c87214fd3
riddle <482825911@qq.com>
2022-05-12 13:04:59 +0800

    feat: 主页的基本结构搭建

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-/*
- * @Author: riddle 482825911@qq.com
- * @Date: 2022-05-09 14:09:10
- * @LastEditors: riddle 482825911@qq.com
- * @LastEditTime: 2022-05-11 23:06:55
- * @FilePath: \geek\src\App.js
- * @Description:
- */
 import 'antd/dist/antd.css'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import Layout from './pages/layout'
+import { Route, Switch } from 'react-router-dom'
+import LayoutPage from './pages/layout'
 import Login from './pages/login'
 function App() {
 	return (
 		<div className='App'>
 			<Switch>
 				<Route path={'/login'} component={Login} />
-				<Route path={'/home'} component={Layout} />
+				<Route path={'/home'} component={LayoutPage} />
 			</Switch>
 		</div>
 	)
--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,6 +1,56 @@
 import { Component } from 'react'
-export default class Layout extends Component {
+import { Layout, Menu } from 'antd'
+import { HomeOutlined } from '@ant-design/icons'
+import styles from './index.module.scss'
+import {
+	LogoutOutlined,
+	FileTextOutlined,
+	EditOutlined,
+} from '@ant-design/icons'
+const { Header, Content, Sider } = Layout
+
+function getItem(label, key, icon, children, type) {
+	return {
+		key,
+		icon,
+		children,
+		label,
+		type,
+	}
+}
+const items = [
+	getItem('数据概览', '1', <HomeOutlined />),
+	getItem('内容管理', '2', <FileTextOutlined />),
+	getItem('发布文章', '3', <EditOutlined />),
+]
+export default class LayoutPage extends Component {
 	render() {
-		return <div>首页</div>
+		return (
+			<Layout className={styles.container}>
+				<Header className='header'>
+					<div className='logo' />
+					<div className='profile'>
+						<span>你好</span>
+						<span>
+							<LogoutOutlined /> 退出
+						</span>
+					</div>
+				</Header>
+				<Layout>
+					<Sider width={200}>
+						<Menu theme='dark' mode='inline' items={items} />
+					</Sider>
+					<Layout
+						style={{
+							padding: '24px',
+						}}
+					>
+						<Content className='site-layout-background'>
+							Content
+						</Content>
+					</Layout>
+				</Layout>
+			</Layout>
+		)
 	}
 }
