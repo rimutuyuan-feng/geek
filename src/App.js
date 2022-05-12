4dda11eab578b5718025db4813d3b9aeca02e2e9
riddle <482825911@qq.com>
2022-05-12 15:06:47 +0800

    chore: BrowserRouter->Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import 'antd/dist/antd.css'
 import { Route, Switch } from 'react-router-dom'
 import LayoutPage from './pages/Layout'
 import Login from './pages/Login'
+import AuthRoute from 'components/AuthRoute'
 function App() {
 	return (
 		<div className='App'>
 			<Switch>
 				<Route path={'/login'} component={Login} />
-				<Route path={'/home'} component={LayoutPage} />
+				<AuthRoute path={'/home'} component={LayoutPage} />
 			</Switch>
 		</div>
 	)
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Router } from 'react-router-dom'
+import history from 'utils/history'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<Router>
+	<Router history={history}>
 		<App />
 	</Router>
 )
