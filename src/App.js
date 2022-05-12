77141bf1a2b2e611f16b4ae998c306cba8d20c72
riddle <482825911@qq.com>
2022-05-12 17:20:34 +0800

    chore: AuthRoute->PrivateRoute

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import 'antd/dist/antd.css'
 import { Route, Switch } from 'react-router-dom'
 import LayoutPage from './pages/Layout'
 import Login from './pages/Login'
-import AuthRoute from 'components/AuthRoute'
+import PrivateRoute from 'components/PrivateRoute'
 function App() {
 	return (
 		<div className='App'>
 			<Switch>
 				<Route path={'/login'} component={Login} />
-				<AuthRoute path={'/home'} component={LayoutPage} />
+				<PrivateRoute path={'/home'} component={LayoutPage} />
 			</Switch>
 		</div>
 	)
