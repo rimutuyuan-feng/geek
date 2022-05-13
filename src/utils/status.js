f43e572783428e0d99cd8639018a0a3f516efe42
riddle <482825911@qq.com>
2022-05-13 11:00:47 +0800

    chore: 添加status常量

---
+++ src/utils/status.js
@@ -0,0 +1,7 @@
+const status = [
+	{ type: '草稿', color: 'magenta' },
+	{ type: '待审核', color: 'orange' },
+	{ type: '审核通过', color: 'green' },
+	{ type: '审核失败', color: 'red' },
+]
+export default status
